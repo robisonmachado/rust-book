70cf3e26bae28fd58096d803ff951e5fa5b808a2
Robison Pereira Machado <robisonpmachado@gmail.com>
2024-05-09 11:59:19 -0300

    feat: 4. Understanding Ownership - 4.1. What is Ownership? - The String Type - OK

---
+++ understanding_ownership/target/debug/deps/understanding_ownership-a6ab76423bdef877.d
@@ -0,0 +1,5 @@
+/home/robison/rust-book/understanding_ownership/target/debug/deps/understanding_ownership-a6ab76423bdef877: src/main.rs
+
+/home/robison/rust-book/understanding_ownership/target/debug/deps/understanding_ownership-a6ab76423bdef877.d: src/main.rs
+
+src/main.rs:
---
+++ understanding_ownership/target/debug/understanding_ownership.d
@@ -0,0 +1,1 @@
+/home/robison/rust-book/understanding_ownership/target/debug/understanding_ownership: /home/robison/rust-book/understanding_ownership/src/main.rs
